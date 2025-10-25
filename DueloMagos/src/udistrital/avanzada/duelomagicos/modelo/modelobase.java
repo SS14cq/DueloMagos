0b93d3d0d4ec2ff212326cee0e624035ded22af1
SS14cq <144471317+SS14cq@users.noreply.github.com>
2025-10-24 23:13:03 -0500

    base duelo entre magos

---
+++ DueloMagos/src/udistrital/avanzada/duelomagicos/modelo/modelobase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udistrital.avanzada.duelomagicos.modelo;
+
+/**
+ *
+ * @author sarit
+ */
+public class modelobase {
+    
+}
